2079bf3b40dbf31f03d8b066d47f81c43a12ba37
Jasmine Yan <jasmineyan@college.harvard.edu>
2014-07-07 12:10:38 -0400

    norvig is too good

--- jlis.py
+++ jlis.py
@@ -4,9 +4,17 @@ z = raw_input()
 
 binop = {'+': op.add, '-': op.sub, '/': op.div, '*': op.mul, '%': op.mod,
         '<': op.lt, '<=': op.le, 'eq?': op.eq, 'neq?': op.ne, '>': op.ge,
-        '>=': op.gt}
+        '>=': op.gt, 'cons': lambda x, y: [x] + [y]}
 
-# string -> token
+unop = {'car': op.add, 'list': lambda *x: list(x)}
+
+class Env(dict):
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+    def find(self, var):
+        return self if var in self else self.outer.find(var)
+
 def tokenize(s):
     return s.replace("(", " ( ").replace(")", " ) ").split()
 
@@ -25,15 +33,31 @@ def parse(t):
     else:
         return atom(token)
 
-def calc(t):
-    if type(t) == list:
-        token = t.pop(0)
-    else:
+def interpret(t, env = Env()):
+    if type(t) == str:
+        return env.find(t)[t] #NOTE do not understand this
+    elif type(t) != list:
+        return t
+    elif t[0] in binop:
+        (_, exp1, exp2) = t
+        e1 = interpret(exp1, env)
+        e2 = interpret(exp2, env)
+        return binop[t[0]](e1, e2)
+    elif t[0] == 'quote':
+        (_, exp) = t
+        return exp
+    elif t[0] == 'cond':
+        (_, test, result, alt) = t
+        return interpret((conseq if eval(test, env) else alt), env)
+    elif t[0] == 'define':
         token = t
     if type(token) == str:
         if token in binop:
+            print t[1]
             return binop[token](calc(t[0]), calc(t[1]))
         # only works for numbers
+        elif token in unop:
+            return unop[token](calc(t[0]))
         elif token == "quote" or token == '\'':
             return t[0]
         elif token == "cons":

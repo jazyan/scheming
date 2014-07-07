fc13d8c5d7c2532712a9d6c708f89069f90147a5
Jasmine Yan <jasmineyan@college.harvard.edu>
2014-07-07 13:15:13 -0400

    it works!

--- jlis.py
+++ jlis.py
@@ -1,18 +1,17 @@
-# TODO: multi-line reading
 import operator as op
-z = raw_input()
 
 binop = {'+': op.add, '-': op.sub, '/': op.div, '*': op.mul, '%': op.mod,
         '<': op.lt, '<=': op.le, 'eq?': op.eq, 'neq?': op.ne, '>': op.ge,
-        '>=': op.gt, 'cons': lambda x, y: [x] + [y]}
+        '>=': op.gt, 'cons': lambda x, y: [x] + y}
 
-unop = {'car': op.add, 'list': lambda *x: list(x)}
+unop = {'car': lambda x: x[0], 'cdr': lambda x: x[1:]}
 
 class Env(dict):
     def __init__(self, parms=(), args=(), outer=None):
         self.update(zip(parms, args))
         self.outer = outer
     def find(self, var):
+        print self, var
         return self if var in self else self.outer.find(var)
 
 def tokenize(s):
@@ -35,6 +34,7 @@ def parse(t):
 
 def interpret(t, env = Env()):
     if type(t) == str:
+        print "hello"
         return env.find(t)[t] #NOTE do not understand this
     elif type(t) != list:
         return t
@@ -43,54 +43,42 @@ def interpret(t, env = Env()):
         e1 = interpret(exp1, env)
         e2 = interpret(exp2, env)
         return binop[t[0]](e1, e2)
+    elif t[0] in unop:
+        (_, exp) = t
+        e = interpret(exp, env)
+        return unop[t[0]](e)
+    elif t[0] == 'list':
+        (_, exp) = t
+        return list(exp)
     elif t[0] == 'quote':
         (_, exp) = t
         return exp
     elif t[0] == 'cond':
         (_, test, result, alt) = t
-        return interpret((conseq if eval(test, env) else alt), env)
+        return interpret((result if eval(test, env) else alt), env)
     elif t[0] == 'define':
-        token = t
-    if type(token) == str:
-        if token in binop:
-            print t[1]
-            return binop[token](calc(t[0]), calc(t[1]))
-        # only works for numbers
-        elif token in unop:
-            return unop[token](calc(t[0]))
-        elif token == "quote" or token == '\'':
-            return t[0]
-        elif token == "cons":
-            if (type(t[1]) == list):
-                li = calc(t[1])
-                li.insert(0, calc(t[0]))
-            else:
-                li = [calc(t[0])]
-                li.append(calc(t[1]))
-            return li
-        # TODO does not account for (car '(1 2 3)), for which returns '
-        # also accepts types other than list
-        elif token == "car":
-            return t[0][0]
-        elif token == "cdr":
-            li = t[0]
-            li.pop(0)
-            return li
-        else:
-            return token
-    elif type(token) == list:
-        return calc(token)
+        (_, var, exp) = t
+        env[var] = interpret(exp, env)
+    elif t[0] == 'lambda':
+        (_, var, exp) = t
+        return lambda *args: interpret(exp, Env(var, args, env))
     else:
-        return token
+        exps = [interpret(exp, env) for exp in t]
+        proc = exps.pop(0)
+        return proc(*exps)
 
 def atom(token):
     try: return int(token)
     except ValueError:
         try: return float(token)
         except ValueError:
-            #TODO: remove double quotes
             return str(token)
-
+'''
 print "TOKENS!", tokenize(z)
 print "PARSE!", parse(tokenize(z))
-print "ANSWER!", calc(parse(tokenize(z)))
+print "ANSWER!", interpret(parse(tokenize(z)))
+'''
+
+while True:
+    val = interpret(parse(tokenize(raw_input('jlis.py> '))))
+    if val is not None: print val
